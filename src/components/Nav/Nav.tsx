64b116fd5dde353a671780f010c54ed028c9b5d3
mousmimukherjee-dev <mousumi.mukherjee@hyperisland.se>
2026-08-16 20:20:32 +0200

    add active category state in navbar

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,12 +2,21 @@
 import { productsData } from "@/data/data";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const Nav = () => {
+
   const pathname = usePathname();
+
+  const[activeCategory , setActiveCategory] = useState("All")
 
   if (pathname === "/cartpage") {
     return null;
+  }
+
+  const handelActiveCategory =(category : string)=>{
+
+    setActiveCategory(category)
   }
 
   const categories = [
@@ -17,13 +26,13 @@ const Nav = () => {
   return (
     <div data-testid="navbar" className="flex font-body justify-between p-6 border-b border-gray-light text-gray-dark">
       <div className="">
-        <ul className="flex justify-start">
+        <ul className="flex justify-start items-center">
           {categories.map((category) => {
             const href =
               category === "All" ? "/products" : `/products/${category}`;
             return (
               <li key={category} className="px-4">
-                <Link href={href}>{category}</Link>
+                <Link href={href} onClick={()=>handelActiveCategory(category)} className={activeCategory === category ? "border-b-2 pb-6.5 border-b-black " : ""}>{category}</Link>
               </li>
             );
           })}
