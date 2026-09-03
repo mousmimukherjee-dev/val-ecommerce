d07d6f51a604c7df547b606df7a6a2909452934e
mousmimukherjee-dev <mousumi.mukherjee@hyperisland.se>
2026-09-03 23:51:21 +0200

    fix nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -6,46 +6,54 @@ import { useState } from "react";
 import { useApiCall } from "@/context/ApiCallContext";
 
 const Nav = () => {
-
   const pathname = usePathname();
-  const { products} = useApiCall();
+  const { products } = useApiCall();
 
-  const[activeCategory , setActiveCategory] = useState("")
+  const [activeCategory, setActiveCategory] = useState("");
 
   if (pathname === "/cartpage") {
     return null;
   }
 
-  if(pathname === "/"){
-
-    return null
+  if (pathname === "/") {
+    return null;
   }
 
-
-  if(pathname === "/signin"){
-
-    return null
+  if (pathname === "/signin") {
+    return null;
   }
 
-  const handelActiveCategory =(category : string)=>{
-
-    setActiveCategory(category)
-  }
+  const handelActiveCategory = (category: string) => {
+    setActiveCategory(category);
+  };
 
   const categories = [
-    "ALL",
+    "All",
     ...new Set(products.map((product) => product.category)),
   ];
   return (
-    <div data-testid="navbar" className="flex font-body justify-between p-6 border-b border-gray-light text-gray-dark">
+    <div
+      data-testid="navbar"
+      className="flex font-body justify-between p-6 border-b border-gray-light text-gray-dark"
+    >
       <div className="">
         <ul className="flex justify-start items-center">
           {categories.map((category) => {
             const href =
-              category === "ALL" ? "/products" : `/products/${category}`;
+              category === "All" ? "/products" : `/products/${category}`;
             return (
               <li key={category} className="px-4">
-                <Link href={href} onClick={()=>handelActiveCategory(category)} className={activeCategory === category ? "border-b-2 pb-6.5 border-b-black " : ""}>{category}</Link>
+                <Link
+                  href={href}
+                  onClick={() => handelActiveCategory(category)}
+                  className={
+                    activeCategory === category
+                      ? "border-b-2 pb-6.5 border-b-black "
+                      : ""
+                  }
+                >
+                  {category}
+                </Link>
               </li>
             );
           })}
